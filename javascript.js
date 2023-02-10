c80c12a5b9959afe0434b98a22a1d9dce553e119
Monica Cao <116030921+monica-c-25@users.noreply.github.com>
2023-02-09 16:21:16 -0800

    Add feature thatallows for previous board to be cleared when user inputs new grid size

--- javascript.js
+++ javascript.js
@@ -1,7 +1,48 @@
+let state = 0;
+
+const startingContainer = document.querySelector('#start');
+
+function startGrid(x) {
+    for (let i = 0; i < x; i++) {
+        let startGrid = startingContainer.appendChild(document.createElement('div'));
+        startGrid.classList.add('startGrid');
+        for (let i = 0; i < x; i++){
+            let boxStart = startGrid.appendChild(document.createElement('div'))
+            boxStart.classList.add('boxStart')
+        }
+    }
+fillStart();
+}
+
+startGrid(10);
+
+function fillStart() {
+    const startBoxes = document.querySelectorAll('.boxStart'); 
+
+    startBoxes.forEach(boxStart => 
+        boxStart.addEventListener('mouseover', colorStart));
+
+    function colorStart() {
+        let color1 = Math.floor(Math.random() * 256);
+        let color2 = Math.floor(Math.random() * 256);
+        let color3 = Math.floor(Math.random() * 256);
+        this.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+    }
+}
+
 const btn = document.getElementById('btn');
 
-btn.addEventListener('click',userInput);
+btn.addEventListener('click',makeGrid);
 
+function makeGrid() {
+    if (state==0) {
+        let board = document.querySelector('#start');
+        board.innerHTML ="";
+        userInput();
+    } else {
+        userInput();
+    }
+}
 
 function userInput() {
     let input = prompt('Enter your grid size', "0");
@@ -12,10 +53,23 @@ function userInput() {
         createGrid(num)
 }
 
-
-const container = document.querySelector('#container');
-
 function createGrid(num) {
+    if (state==0){
+        document.body.removeChild(startingContainer);
+        let container = document.createElement('div');
+        container.id = 'container';
+        document.body.appendChild(container);
+            for (let i = 0; i < num; i++) {
+                let boxContainer = container.appendChild(document.createElement('div'));
+                boxContainer.classList.add('boxContainer');
+                for (let i = 0; i < num; i++){
+                    let box = boxContainer.appendChild(document.createElement('div'))
+                 box.classList.add('box')
+                }
+             }
+    } else {
+        const container = document.getElementById('container');
+        container.innerHTML = '';
         for (let i = 0; i < num; i++) {
             let boxContainer = container.appendChild(document.createElement('div'));
             boxContainer.classList.add('boxContainer');
@@ -24,10 +78,12 @@ function createGrid(num) {
                 box.classList.add('box')
             }
         }
+    }
     fill();
 }
 
 function fill() {
+    state = 1;
     const boxes = document.querySelectorAll('.box'); 
 
     boxes.forEach(box => 
@@ -38,8 +94,10 @@ function fill() {
         let color2 = Math.floor(Math.random() * 256);
         let color3 = Math.floor(Math.random() * 256);
         this.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
+    
     }
 }
+
 
 
 const reset = document.getElementById('reset');
@@ -49,6 +107,8 @@ reset.addEventListener('click', newBoard);
 function newBoard() {
     location.reload();
 }
+
+
 
 
 
