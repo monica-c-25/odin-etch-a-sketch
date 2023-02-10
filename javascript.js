240e984de855f8672e856e265fd1a7c1d204c6f5
Monica Cao <116030921+monica-c-25@users.noreply.github.com>
2023-02-10 14:52:43 -0800

    Refine clear board button function and add CSS styling

--- javascript.js
+++ javascript.js
@@ -11,23 +11,22 @@ function startGrid(x) {
             boxStart.classList.add('boxStart')
         }
     }
-fillStart();
+    fillStart();
 }
 
-startGrid(10);
+startGrid(20);
 
 function fillStart() {
     const startBoxes = document.querySelectorAll('.boxStart'); 
-
     startBoxes.forEach(boxStart => 
         boxStart.addEventListener('mouseover', colorStart));
+}
 
-    function colorStart() {
-        let color1 = Math.floor(Math.random() * 256);
-        let color2 = Math.floor(Math.random() * 256);
-        let color3 = Math.floor(Math.random() * 256);
-        this.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
-    }
+function colorStart() {
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
+    this.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
 }
 
 const btn = document.getElementById('btn');
@@ -47,8 +46,9 @@ function makeGrid() {
 function userInput() {
     let input = prompt('Enter your grid size', "0");
     let num = parseInt(input);
-    if (num > 100) {
-        alert('Enter a number less than 100!')
+    if (num > 100 || num <= 0) {
+        alert('Enter a number less than 100 but greater than 0!')
+        startGrid(20);
     } else 
         createGrid(num)
 }
@@ -88,24 +88,38 @@ function fill() {
 
     boxes.forEach(box => 
         box.addEventListener('mouseover', color));
+}
 
-    function color() {
-        let color1 = Math.floor(Math.random() * 256);
-        let color2 = Math.floor(Math.random() * 256);
-        let color3 = Math.floor(Math.random() * 256);
-        this.style.backgroundColor = `rgb(${color1},${color2},${color3})`;
-    
-    }
+function color() {
+    let color1 = Math.floor(Math.random() * 256);
+    let color2 = Math.floor(Math.random() * 256);
+    let color3 = Math.floor(Math.random() * 256);
+    this.style.backgroundColor = `rgb(${color1},${color2},${color3})`; 
 }
 
 const reset = document.getElementById('reset');
 
 reset.addEventListener('click', clearBoard);
 
-function clearBoard() {
-    let divs = document.querySelectorAll("div");
-    divs.forEach((div) => div.style.backgroundColor= "white")
+function colorChange(element) {
+    element.style.backgroundColor = "white";
 }
+
+function clearBoard() {
+        const nodeList = document.querySelectorAll(".boxStart");
+        Array.from(nodeList).forEach(colorChange);
+        const nodeListTwo = document.querySelectorAll(".box");
+        Array.from(nodeListTwo).forEach(colorChange);
+}
+
+ 
+
+
+
+
+
+
+
 
 
 
